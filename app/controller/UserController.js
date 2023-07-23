9bcd12f7ae7e37e2b5ca3a8beb552321a9603c8b
Mohammadreza Mousazadeh <mohammadreza.pars18@gmail.com>
2023-07-23 17:22:35 +0330

    added handle login along side with jwt package

--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -1,8 +1,11 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const UserModel = require('../model/User');
 const {errorCreator} = require('../utils/error');
 const { sendMail } = require('../utils/mailer');
 
-exports.registerUser = async(req, res, next)=>{
+exports.registerHandler = async(req, res, next)=>{
     try {
         await UserModel.userValidation(req.body);
         const {email, fullname, phone, password} = req.body;
@@ -14,10 +17,24 @@ exports.registerUser = async(req, res, next)=>{
             phone,
             password
         });
-        sendMail(email, fullname, 'Welcome to our community', 'You have been registered to our system')
+        // sendMail(email, fullname, 'Welcome to our community', 'You have been registered to our system')
         res.status(201).json({message : 'User Has Been Registered'});
     } catch (err) {
         console.log(err)
+        next(err)
+    }
+}
+
+exports.loginHandler = async(req, res, next)=>{
+    try {
+        const {email, password} = req.body;
+        const user = await UserModel.findOne({email});
+        if(!user) throw errorCreator('User Not Found!', 404);
+        const comparePass = await bcrypt.compare(password, user.password);
+        if(!comparePass) throw errorCreator('Either Email or Password is Wrong!', 422)
+        const token = jwt.sign({user : user._id.toString(), fullname : user.fullname, email : user.email }, process.env.JWT_SECRET);
+        res.status(200).json({token, user: user._id.toString(), message : 'Welcome'})
+    } catch (err) {
         next(err)
     }
 }
--- app/routes/UserRoutes.js
+++ app/routes/UserRoutes.js
@@ -5,6 +5,9 @@ const UserController = require('../controller/UserController');
 const router = new Router();
 
 //*     @desc   POST    /user/register
-router.post('/register', UserController.registerUser)
+router.post('/register', UserController.registerHandler)
+
+//*     @desc   POST    /user/login
+router.post('/login', UserController.loginHandler)
 
 module.exports = router;
